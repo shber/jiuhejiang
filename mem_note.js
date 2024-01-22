5f854210714ad02ab8d3c527582de8bfee65a029
Shber <web648@126.com>
2024-01-22 21:42:51 +0800

    1

---
+++ mem_note.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: Shber
+ * @Date: 2023-12-12 12:29:41
+ * @LastEditors: Shber
+ * @LastEditTime: 2023-12-15 19:10:57
+ * @Description: 
+ */
+
+
+
+// I am writing this letter for the purpose of applying for the post
+// mentioned in your advertisement on the internet.
+// I graduated from Peking University majoring in MEM, 
+// I have therefore attained professional knowledgein this field.
